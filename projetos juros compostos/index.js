dda1496991fcd97b7065d61d9c8dffb2671a6b85
Felps1532 <felipebelomi@gmail.com>
2025-04-11 20:31:00 -0300

    aVANCEI BASTANTE

--- projetos juros compostos/index.js
+++ projetos juros compostos/index.js
@@ -6,15 +6,30 @@ function calcularResultados() {
     let mesInicial = document.getElementById('input-mesInicial').value;
     let anoInicial = document.getElementById('input-anoInicial').value;
 
-    document.getElementById('tbl-resultados').innerHTML = 
-    "<table class='tbl-titulos'>" +
-        "<tr>" +
-            "<td>Sequência</td>" +
-            "<td>Mês</td>" +
-            "<td>Ano</td>" +
-            "<td>Valor interior</td>" +
-            "<td>Rendimento</td>" +
-            "<td>Valor final</td>" +
-        "</tr>" +
-    "</table>";
+    // TESTE DOS VALORES DOS INPUTS
+    document.getElementById('resultados').innerHTML = valorInicial + " | " +
+        investMensal + " | " + jurosAno + " | " + numMeses + " | " + mesInicial + " | " +
+        anoInicial;
+
+    for (let i; i <= numMeses; i++) {
+        // document.getElementById('resultados').innerHTML = 
+        //     "<table class='tbl-titulos'>" +
+        //         "<tr>" +
+        //             "<td>Sequência</td>" +
+        //             "<td>Mês</td>" +
+        //             "<td>Ano</td>" +
+        //             "<td>Valor interior</td>" +
+        //             "<td>Rendimento</td>" +
+        //             "<td>Valor final</td>" +
+        //         "</tr>" +
+        //     "</table>";
+    }
+
+    
+
 } 
+
+
+function reset() {
+    document.getElementById('resultados').innerHTML = "";
+}
